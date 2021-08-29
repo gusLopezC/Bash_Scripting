719300b9949d79780d845cf1415db54b3838a137
gusLopezC <guslopezcallejas@gmail.com>
2021-08-29 05:29:55 -0500

    Create 02.Variables.sh

---
+++ 02.Variables.sh
@@ -0,0 +1,18 @@
+# Your code here!
+
+NAME="Gustavo"
+YEAR=2021
+TEXT="Hola desde queretaro"
+
+
+echo "Hola $NAME"
+echo "El anio es $YEAR"
+echo "$TEXT"
+
+NAME="GUS" 
+echo "Hola $NAME"
+
+NAME="LOPEZ" 
+readonly NAME 
+
+echo "Hola $NAME"
